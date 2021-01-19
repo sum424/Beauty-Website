4fa08882cba11bec51c86a1a03e403d31e544170
Sum424 <sumanthapa326@gmail.com>
2021-01-20 00:46:39 +0545

    seventh commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 $(document).ready(() => { 
     $(".img-slider").responsiveSlides();
+
+    //for head nav
+    $(window).on("scroll",function() {
+        if($(window).scrollTop() > 0) {
+            $(".header").addClass("sticky");
+        } 
+        else {
+            $(".header").removeClass("sticky");
+        }
+    });
 });
